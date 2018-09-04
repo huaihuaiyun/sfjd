710faede475fcae55b6f39e789a4f8b896d88b05
yunsir <yunsir@qq.com>
2018-09-04 22:17:26 +0800

    9.4

--- js/size.js
+++ js/size.js
@@ -4,16 +4,38 @@
 //     $(".content").css("height",aHeight+"px");
 //     $(".right_main").css("width",aWidth+"px");
 // }
-var aHeight = $(window).height();
+//页面高度
+var dHeight = $(document).height();
+console.log(dHeight);
+//浏览器窗口高度
+var wHeight = $(window).height();
+console.log(wHeight);
+//浏览器窗口宽度
 var aWidth = $(window).width();
-function setSize(selector,attr,size){
-    if(attr == 'height'){
-        $(selector).css(attr,(aHeight-size)+'px');
-    }else if(attr == 'width'){
-        $(selector).css(attr,(aWidth-size)+'px');
-    }
+var rightMainHeight = $(".right_main").outerHeight()+50;
+console.log(rightMainHeight);
+
+if((rightMainHeight+105) > wHeight){
+    $(".left_aside").height(rightMainHeight);
+}else{
+    $(".left_aside").height(wHeight-105);
 }
-setSize(".content",'height',105);
-setSize(".right_main",'width',290);
-setSize(".content_main",'height',155);
+$(".right_main").width(aWidth-290);
 $(".info_list").css("width",($(".info_box_cont").width()-150)+"px");
+// function setSize(selector,attr,size){
+//     if(attr == 'height'){
+//         $(selector).css(attr,(aHeight-size)+'px');
+//     }else if(attr == 'width'){
+//         $(selector).css(attr,(aWidth-size)+'px');
+//     }
+// }
+// // if($(".right_main").height() > (aHeight-105)){
+// //     console.log($(".right_main").width());
+    
+// //     $(".content").css("height",$(".right_main").height()+"px");
+// //     // setSize(".content",'height',105);
+// // }else{
+// //     setSize(".content",'height',105);
+// // }
+// setSize(".right_main",'width',290);
+// 
